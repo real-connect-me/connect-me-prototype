52f4e00e71b851c594d2dc80300b7ef8156fdf46
helooo1233 <71606730+helooo1233@users.noreply.github.com>
2022-12-19 18:11:32 -0500

    pages in url

--- homepage.py
+++ homepage.py
@@ -2,16 +2,16 @@
 from flask import Flask, request, url_for, render_template, redirect
 
 app = Flask(__name__)
-app.config.from_prefixed_env()
+# app.config.from_prefixed_env()
 # events = os.environ["events"] # list full of dictionaries with event data
 events = []
-start = 0
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
+    page = request.args.get("page", default = 0, type = int)
     if request.method == "POST":
         events.append({"name":request.form["Event Name"], "place":request.form["Event Place"], "time":request.form["Event Time"]})
-    return render_template("home.html", evs=events, s=start)
+    return render_template("home.html", evs=events, page=page)
 
 @app.route("/event-create")
 def eventform():

70564798c294c32bd251f97f06ada212722f1174
helooo1233 <71606730+helooo1233@users.noreply.github.com>
2023-01-14 13:31:53 -0500

    save

--- C_Prototype/forms.py
+++ C_Prototype/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
-class SearchForm():
-    keywords = StringField("Enter keywords")
-    submit = SubmitField("Go")
+class SearchForm(FlaskForm):
+    keywords = StringField("Enter Keywords", validators=[DataRequired()])
+    submit = SubmitField("")
